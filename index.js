42ba838651e8a349dc8c4d4c25773d23ed1c85c5
nileshbandal20 <36812293+nileshbandal20@users.noreply.github.com>
2025-10-07 20:31:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,18 +22,20 @@ const checkAvailability = async () => {
       ...partParams
     },
     headers: {
-        "Accept": "*/*",
-        "Accept-Language": "en-US,en;q=0.9",
-		"priority" :"u=1, i",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
-        "Referer": "https://www.apple.com/in/shop/buy-iphone",
-        "sec-ch-ua": "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "x-aos-ui-fetch-call-1": "9vp7kp6n89-mgfge6xl",
-        "Cookie": "dssid2=33e80e98-29e9-4933-ab60-0507bf944dda; dssf=1; geo=IN; s_campaign=aos-in-kwgo-brand--; s_cc=true; as_rumid=307bc8fe-9309-4657-abf8-26ba0fa33a74; as_uct=0; as_dc=ucp5; sh_spksy=.; s_fid=04D8C7CD5ECA54A1-01D373DBE48DCF09; s_vi=[CS]v1|3472017951B3F4AE-60001C2A404BA09D[CE]; shld_bt_ck=z_k2Sq2kjdpkvpifZ4p7Sw|1759780635|gT1sKGEXuw94qPJZueZlzs7coTbGjXmfDaAPqTc7A7BiyBzmstgNlpROxeriRtdSPBBJyakjdaf3mC4qEPcTk4EKsynjV6mD0azpqj7M-f-vupEzeRMXJP7q71tQSdKXOiuiSOVpdz3nwvCwJi1i-t2raOfhC-orcmC6XcnMDM7IsRJEIyQ58Uq2uWjv3JmPPLyEkVx587qIqcHURwl76rIp5x5nd5ll2WQkz9P-vBk4P9iHWcB26CcoK-656WqzlcTB5z_u9__7-CrSD1L1jU6pID60AIkgD4KXEzfLdxEN-hPgx4oXNUlF01g6KwVBYgZmBVFxEsU042g330puDw|CMNQ5Drr4rG70GPGjka3AR79NEc; as_pcts=g21JMZQXO6QRU5PI7fxWwVmvnT1fnR1XNeZOgqyMwhesMINItJAc1NajZOgCYs6xYoEzHaXjZixO-Gg9zVY1tINCB1tb8HjoNLxltukGBzMasnra+IlW6iZeCGsDPXbRohVqC0Hgs738yByg:VRktoomwkjRhKVG4Vznbz:bAuDGfj8mzn4; as_affl=p240%7C%7Cken_pid%3A%3Ago~cmp-11116556120~adg-109516736339~ad-774710206782_kwd-297832030443~dev-c~ext-~prd-~mca-~nt-search%26cid%3A%3Aaos-in-kwgo-brand--%26token%3A%3A8c1a76ab-cfe5-4380-b551-e755a3b48bc8%26%7C%7C20251006_110000; as_sfa=Mnxpbnxpbnx8ZW5fSU58Y29uc3VtZXJ8aW50ZXJuZXR8MHwwfDE; s_afc=p240%7Cgo%7Ecmp-11116556120%7Eadg-109516736339%7Ead-774710206782_kwd-297832030443%7Edev-c%7Eext-%7Eprd-%7Emca-%7Ent-search; as_gloc=1b5eac2d18d706927094fbf88b1f3fd4427268b0bd5ceff38776461ccd6f81450082738b31668cd4eea1253464a4761cd4360d21249636e9f7fb925bdec0eee1654d19b1b22c847d6539baf0295fbc6a41392f29a4602019f2c4c9abd660faa2; as_atb=1.0|MjAyNS0xMC0wNiAxMToxNzoxNg|207eb53417bce80efd88ece0d2e3d153f1b20afe; shld_bt_m=ICUHXetK7NX_HZY1FyX_1Q|1759781855|E7P4xXMTntOgZ_dKnFQVbA|a7Js7FE7EA9Pfb_YYzgQj6E_uZU; s_sq=%5B%5BB%5D%5D"
-      }
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9,mr;q=0.8",
+    "priority": "u=1, i",
+    "sec-ch-ua": "\"Google Chrome\";v=\"141\", \"Not?A_Brand\";v=\"8\", \"Chromium\";v=\"141\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-aos-ui-fetch-call-1": "iul8wm5wk7-mggokxqb",
+    "x-skip-redirect": "true",
+    "cookie": "dssf=1; dssid2=780b5bd9-89e4-41d8-9d01-378275cf95b1; as_uct=0; pxro=1; as_disa=AAAjAAABc4tCej5oWyHwqE-Qu79T9d79m0pDNjb7mtDze2q9tMH1njBAQUROfcQ3gZCoYBWiAAIBRlezInO8qpjSdRbk6lSfqR7tOxeLYVXMtKej9x6EFUg=; geo=IN; s_cc=true; as_rumid=80a1fcd5-95b3-4f67-94a2-1ba6dc358550; as_sfa=Mnxpbnxpbnx8ZW5fSU58Y29uc3VtZXJ8aW50ZXJuZXR8MHwwfDE; dslang=IN-EN; site=IND; rtsid=%7BIN%3D%7Bt%3Da%3Bi%3DR744%3B%7D%3B%7D; as_loc=68b6ab88494ca040dc241f9f85f96c8544358daf4459c32192a8787355a80fd4ecb90b97f325afb6f2f6602edf601be5a79f2d697e121702bf5274483976da434fdc14eacbb72725a66d93ba4105ef4f32ed641266b34a269528b09aed47a4eb; as_pcts=hUw9gdUiwxkercfASr5w2Lj-o496kaXQvxtrH9up2GefupIKwqbGpqXMJSfY5UhTAgWaocvG1w3BaSF41kzQXzvAhI0NMSWRxfhP8cD9s8lEzz; s_fid=730A1C69598C4777-14AB9141EAD18E3F; as_dc=ucp5; sh_spksy=.; s_vi=[CS]v1|347294CE117B8830-60001CA961C0D8B0[CE]; s_sq=%5B%5BB%5D%5D; as_atb=1.0|MjAyNS0xMC0wNyAwNzo1NDoyMg|f2dcda9fcbb9f9df49f6df9f590996e9943082f4; shld_bt_ck=cBcbjy_QpD7r4kY3luaexQ|1759856066|zDOXZTBccLY6fO99qfYfSb6Gz3aTtXluCosLPYK_OVSJswwj7CGwJnZkUWndkKYBrG7VfQCIqhoGbxrATpDR1jtd6IF8vOgZ8LtUCT0bqKpTKRR3QY65bK3LVhSS7M-4ihl0tC1GTqhXhBQF_s8g3Mq5yZviTvbXL8_7nMSi2bMvC8d0r9MfhcFebSzzA1MV8ckN29T3lsQPWi5rsORjOvY0pVB12_EypeLhtnEI_T20mSzUU0eQLjn1Nd6XbRy6WW9SosNG0jMpy6zwzN_yMlr0_NyIDTTQ6ywiNq6fzo6mzAQOIzeXRblzSs4T_jUK7DVs82c_0CwrtCK2VcmFTw|e_q2IqTZmHupNJXPIAVq4afJYko; shld_bt_m=43hV97TxyDE3i1cpgbGNoQ|1759856071|c9xAq0QfLbL_JIxKFm2vzA|CVZKwMsPSDvk4RCFrzZr2tKEuKw",
+    "Referer": "https://www.apple.com/in/shop/buy-iphone/iphone-17-pro"
+  }
   });
 
   const stores = data.body.content.pickupMessage.stores;
